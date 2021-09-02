3dbeb3cb480acdee80b6a09fb48963a0d8c6f7d5
Aamir Mughal <aaamirmughal@gmail.com>
2021-09-02 16:08:53 +0500

    Mailer Update in Production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,20 +32,18 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.zoho.com',
-    domain: 'devbox.co',
+    address: "smtp.gmail.com",
     port: 587,
-    user_name: 'ameer.hamza@devbox.co',
-    password: '24421591Hamza+',
-    authentication: 'plain',
-    enable_starttls_auto: true
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "email.from.devbox@gmail.com",
+    password: "$devbox123$",
+    domain: "smtp.gmail.com"
   }
 
   config.action_mailer.perform_caching = false
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'ameer.hamza@devbox.co'
+  config.mailer_sender = 'email.from.devbox@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
