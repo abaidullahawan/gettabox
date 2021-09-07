7e6d0c0a177325a63e8370f7fb1e018fd0dfb776
Ameer Hamza <ameer.hamza@devbox.co>
2021-09-07 20:40:32 +0500

    total cost sum with js

--- app/javascript/packs/purchase_order.js
+++ app/javascript/packs/purchase_order.js
@@ -4,4 +4,21 @@ $(document).on('turbolinks:load', function () {
   $('.order_item_price').on('blur', function () {
     $('.supplier-submit').trigger('click')
   })
+  $('.order_item_price, .order_item_quantity').on('keyup', function () {
+    var name = this.id
+    var quantity_field = this.id.replace('quantity', 'cost_price')
+    var cost_price_field = this.id.replace('cost_price', 'quantity')
+    var quantity = $("#" + quantity_field).val()
+    var cost = $("#" + cost_price_field).val()
+    var item_total = quantity * cost
+
+    $(this).closest('tr').find('.order_item_total').val(item_total)
+    debugger;
+    var sum = 0;
+    $('.order_item_total').each(function () {
+      sum += parseFloat($(this).val());  // Or this.innerHTML, this.innerText
+    });
+    $('.order_total').val(sum)
+
+  })
 })
