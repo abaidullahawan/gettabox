c3f12fbdbe297f5930f59a55544e7aace29d2d76
Aamir Mughal <aaamirmughal@gmail.com>
2021-09-07 19:38:10 +0500

    Purchase Delivery CRUD

--- app/models/purchase_delivery.rb
+++ app/models/purchase_delivery.rb
@@ -2,4 +2,6 @@ class PurchaseDelivery < ApplicationRecord
   belongs_to :purchase_order
   has_many :purchase_delivery_details
 
+  accepts_nested_attributes_for :purchase_delivery_details, allow_destroy: true
+
 end
