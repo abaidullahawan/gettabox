9f5f8544533181955a7d6ac50caa390e0b2a4501
Ameer Hamza <ameer.hamza@devbox.co>
2021-09-08 14:15:09 +0500

    Total sum in purchase orders

--- app/javascript/packs/purchase_order.js
+++ app/javascript/packs/purchase_order.js
@@ -1,24 +1,31 @@
 import $ from 'jquery'
 
 $(document).on('turbolinks:load', function () {
+  purchaseOrderTotal()
   $('.order_item_price').on('blur', function () {
     $('.supplier-submit').trigger('click')
   })
-  $('.order_item_price, .order_item_quantity').on('keyup', function () {
-    var name = this.id
-    var quantity_field = this.id.replace('quantity', 'cost_price')
-    var cost_price_field = this.id.replace('cost_price', 'quantity')
-    var quantity = $("#" + quantity_field).val()
-    var cost = $("#" + cost_price_field).val()
-    var item_total = quantity * cost
 
-    $(this).closest('tr').find('.order_item_total').val(item_total)
-    debugger;
+  $('.order_item_price, .order_item_quantity').on('keyup', function () {
+    purchaseOrderTotal()
+  })
+
+})
+function purchaseOrderTotal() {
+  var length = $('.order_item_price').length
+  for (var i = 0; i < length; ++i) {
+    var cost_price_id = "purchase_order_purchase_order_details_attributes_" + i + "_cost_price"
+    var quantity_id = "purchase_order_purchase_order_details_attributes_" + i + "_quantity"
+    var quantity = $("#" + quantity_id).val()
+    var cost = $("#" + cost_price_id).val()
+    var item_total = (isNaN(parseFloat(quantity) * parseFloat(cost))) ? 0 : parseFloat(quantity) * parseFloat(cost);
+    $("#" + quantity_id).closest('tr').find('.order_item_total').val(item_total)
+
     var sum = 0;
     $('.order_item_total').each(function () {
-      sum += parseFloat($(this).val());  // Or this.innerHTML, this.innerText
+      var value = (isNaN(this.value)) ? 0 : this.value;
+      sum += parseFloat(value);  // Or this.innerHTML, this.innerText
     });
     $('.order_total').val(sum)
-
-  })
-})
+  }
+}
