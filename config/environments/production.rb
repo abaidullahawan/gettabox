e759171dc0adde45e2677335e798fa422d4c5187
Aamir Mughal <aaamirmughal@gmail.com>
2021-10-04 15:56:33 +0500

    Mailer Configra

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,10 +99,10 @@ Rails.application.configure do
   # window Computer PW: dhyqjlwnezfvtpel
   # Mac Computer PW: layascwrwthtlbsw
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.zoho.com",
+    :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "mohammad.haris@devbox.co",
-    :password             => "harris..,,12345",
+    :user_name            => "munchionclick@gmail.com",
+    :password             => "munchi@123",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
