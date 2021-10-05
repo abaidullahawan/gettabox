1dfafe364b6f00070351e875c052b58066b99e40
Ameer Hamza <ameer.hamza@devbox.co>
2021-10-05 14:07:08 +0500

    Product mapping for production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_113101) do
+ActiveRecord::Schema.define(version: 2021_10_04_115834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,11 +107,9 @@ ActiveRecord::Schema.define(version: 2021_09_29_113101) do
 
   create_table "extra_field_names", force: :cascade do |t|
     t.json "field_name"
-    t.string "fieldnameable_type"
-    t.bigint "fieldnameable_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["fieldnameable_type", "fieldnameable_id"], name: "index_extra_field_names_on_fieldnameable"
+    t.string "table_name"
   end
 
   create_table "extra_field_values", force: :cascade do |t|
@@ -201,9 +199,9 @@ ActiveRecord::Schema.define(version: 2021_09_29_113101) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
+    t.integer "product_type"
     t.bigint "season_id"
     t.text "description"
-    t.integer "product_type"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["deleted_at"], name: "index_products_on_deleted_at"
     t.index ["season_id"], name: "index_products_on_season_id"
