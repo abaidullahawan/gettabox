3d7493dd2e54e6afae50828bb8fab7dc03113fe1
abaidullahawan <basharat.senior1@gmail.com>
2021-10-29 17:05:38 +0500

    Revert "api calls without sidekiq job"

--- app/controllers/product_mappings_controller.rb
+++ app/controllers/product_mappings_controller.rb
@@ -32,72 +32,8 @@ class ProductMappingsController < ApplicationController
   end
 
   def all_order_data
-    @page_no = 1
-    @total_pages = 1
-    refresh_token = RefreshToken.last
-
-    require 'net/http'
-    require 'base64'
-    require 'http'
-    require 'json'
-    require 'active_support/core_ext'
-    require "rexml/document"
-    require 'builder'
-
-    loop do
-      @xml_data = Builder::XmlMarkup.new()
-      @xml_data.instruct!
-      @xml_data.GetMyeBaySellingRequest("xmlns" => "urn:ebay:apis:eBLBaseComponents"){
-        @xml_data.RequesterCredentials{
-          @xml_data.eBayAuthToken "#{refresh_token.access_token}"
-        }
-        @xml_data.ErrorLanguage "en_US"
-        @xml_data.WarningLevel "High"
-        @xml_data.ActiveList{
-          @xml_data.Sort "TimeLeft"
-          @xml_data.Pagination{
-            @xml_data.PageNumber "#{@page_no}"
-          }
-        }
-      }
-      response = HTTP.post("https://api.ebay.com/ws/api.dll",
-        :body => @xml_data.to_xml.split("<inspect/>").first,
-        :headers => {
-          'X-EBAY-API-SITEID' => '3',
-          'X-EBAY-API-COMPATIBILITY-LEVEL' => '967',
-          'X-EBAY-API-CALL-NAME' => 'GetMyeBaySelling',
-          'X-EBAY-API-APP-NAME' => 'ChannelD-ChannelD-PRD-da28ec690-4a9f363c',
-          'X-EBAY-API-DEV-NAME' => '8a3c7cee-7507-45ca-bb47-22ffe194a94b',
-          'X-EBAY-API-CERT-NAME' => 'PRD-a28ec6908ea9-7c43-4fd9-be43-0e7d',
-          'X-EBAY-API-DETAIL-LEVEL' => '0'
-        }
-      )
-      @xml_response_data = Nokogiri::XML(response.body)
-      @data_xml_re = Hash.from_xml(@xml_response_data.to_xml)
-      @total_pages = @data_xml_re['GetMyeBaySellingResponse']['ActiveList']['PaginationResult']['TotalNumberOfPages'].to_i
-      ChannelResponseData.create(channel: "ebay", response: @data_xml_re, api_url: "https://api.ebay.com/ws/api.dll", api_call: "GetMyeBaySelling", status: "panding")
-      @page_no += 1
-      if @page_no > @total_pages
-        break
-      end
-    end
-    @response_items = ChannelResponseData.all
-    @response_items.each do |response_item|
-      if ((response_item.api_call == "GetMyeBaySelling") && (response_item.status == "panding"))
-        response_item.response['GetMyeBaySellingResponse']['ActiveList']['ItemArray']['Item'].each do |item|
-          if item['Variations'].present?
-            item['Variations']['Variation'].each do |variation|
-              ChannelProduct.create_with(channel_type: 'ebay', item_id: item["ItemID"].to_i, product_data: item, item_sku: variation["SKU"]).find_or_create_by(channel_type: 'ebay', item_id: item["ItemID"].to_i, item_sku: variation["SKU"])
-            end
-          else
-            ChannelProduct.create_with(channel_type: 'ebay', item_id: item["ItemID"].to_i, product_data: item, item_sku: item["SKU"]).find_or_create_by(channel_type: 'ebay', item_id: item["ItemID"].to_i)
-          end
-        end
-        response_item.update(status: "executed")
-      end
-    end
-    # CreateChannelProductResponseJob.perform_later
-    flash[:notice] = 'Products call fullfilled successfully'
+    CreateChannelProductResponseJob.perform_later
+    flash[:notice] = "Call sent to eBay API"
     redirect_to product_mappings_path
   end
 
