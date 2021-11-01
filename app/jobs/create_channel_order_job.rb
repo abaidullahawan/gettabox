87d3564eca7c75367cce13d5c46dc4f3785ff5d3
Ameer Hamza <ameer.hamza@devbox.co>
2021-11-01 11:55:36 +0500

    Refresh Token api code refactor

--- app/jobs/create_channel_order_job.rb
+++ app/jobs/create_channel_order_job.rb
@@ -8,10 +8,11 @@ class CreateChannelOrderJob < ApplicationJob
     @response_orders = ChannelResponseData.all
     @response_orders.each do |response_order|
       next unless (response_order.api_call == 'getOrders') && (response_order.status == 'panding')
+
       response_order.response['orders'].each do |order|
         creationdate = order['creationDate']
         channel_order_record = ChannelOrder.find_or_initialize_by(ebayorder_id: order['orderId'],
-                                                                channel_type: 'ebay')
+                                                                  channel_type: 'ebay')
         channel_order_record.order_data = order
         channel_order_record.created_at = creationdate
         channel_order_record.save
