a545c555071b9cb0f93218ce6b5d2216af2e2dab
IAMSNRAZA <ahsanraxae32@gmail.com>
2021-11-15 15:27:05 +0500

    Shoaibs tasks mapping left only

--- app/javascript/packs/product_mapping.js
+++ app/javascript/packs/product_mapping.js
@@ -28,6 +28,34 @@ $(document).on('turbolinks:load', function () {
     $('#single-product-create-modal').modal('show')
   })
 
+  $('.create-single-product-mapping').on('click', function () {
+    var json = this.dataset.item
+    var cd_id = this.dataset.id
+    var data = JSON.parse(json)
+    debugger
+    var sku = data[0].sku
+    var title = data[0].item_data.title
+    // var description = data.product_data.description
+    // var imageUrl = data[0].item_data.product_data.PictureDetails.GalleryURL
+    // var urlSplit = imageUrl.split('/')
+    // var imageName = urlSplit[urlSplit.length - 1]
+    // var quantity = data.product_data.Quantity
+
+    $('#single-product-create-modal .modal-body #channel_product_id').val(cd_id)
+    $('#single-product-create-modal .modal-body #product_title').val(title)
+    $('#single-product-create-modal .modal-body #product_sku').val(sku)
+    // $('#single-product-create-modal .modal-body #product_description').val(description)
+    // $('#single-product-create-modal .modal-body #product_photo').attr("src", imageUrl)
+    // $('#single-product-create-modal .modal-body #product_photo').attr("filename", imageName)
+    // $('#single-product-create-modal .modal-body #product_total_stock').val(quantity)
+    // if(imageName  !== '' )
+    // {
+    //   $('#single-product-create-modal .modal-body #photo-label').text(`Photo ${imageName} Attached`)
+    // }
+    $('#single-product-create-modal').modal('show')
+  })
+
+
   $('.create-multi-product').on('click', function () {
     var json = this.dataset.item
     var cd_id = this.dataset.id
@@ -49,11 +77,13 @@ $(document).on('turbolinks:load', function () {
     $(this).closest('td').find('.productSearch').trigger( "focus" )
   })
 
-  $('.productSearch').on('focusout', function () {
-    $(this).closest('td').find('.productSearchContainer').toggleClass('d-none', 3000);
-    $(this).closest('td').find('.productSearch').toggleClass('d-none', 3000);
-  })
-
+  // $('.productSearch').on('focusout', function () {
+  //   if ($('.productSearch').value === ''){
+  //     debugger
+  //     $(this).closest('td').find('.productSearchContainer').toggleClass('d-none', 3000);
+  //     $(this).closest('td').find('.productSearch').toggleClass('d-none', 3000);
+  //   }
+  // })
   $('#q_status_eq, #product_mapping').on('change', function () {
     $('.product-mapping-request').trigger('click')
   })
