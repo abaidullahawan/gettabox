dcf46c188f671eddcec737a5b3c684e51b3cf9f0
IAMSNRAZA <ahsanraxae32@gmail.com>
2021-11-23 13:13:59 +0500

    Export sett and Product job sett

--- app/jobs/create_channel_product_job.rb
+++ app/jobs/create_channel_product_job.rb
@@ -17,20 +17,21 @@ class CreateChannelProductJob < ApplicationJob
   end
 
   def create_or_update_product(item)
-    return multi_product(item, item['Variations']['Variation']) if item['Variations'].present?
+    picture = item['PictureDetails'].present? ? item['PictureDetails']['GalleryURL'] : nil
+    return multi_product(item, item['Variations']['Variation'], picture) if item['Variations'].present?
 
     ChannelProduct.create_with(channel_type: 'ebay', item_id: item['ItemID'].to_i,
                                product_data: item, item_sku: item['SKU'], item_quantity: item['Quantity'],
-                               item_image: item['PictureDetails']['GalleryURL'], item_name: item['Title'])
+                               item_image: picture, item_name: item['Title'])
                   .find_or_create_by(channel_type: 'ebay', item_id: item['ItemID'].to_i)
   end
 
-  def multi_product(item, variations)
+  def multi_product(item, variations, picture)
     variations.each do |variation|
       ChannelProduct.create_with(channel_type: 'ebay', item_id: item['ItemID'].to_i,
                                  product_data: item, item_sku: variation['SKU'],
                                  item_quantity: variation['Quantity'],
-                                 item_image: item['PictureDetails']['GalleryURL'],
+                                 item_image: picture,
                                  item_name: variation['VariationTitle'])
                     .find_or_create_by(channel_type: 'ebay', item_id: item['ItemID'].to_i, item_sku: variation['SKU'])
     end
