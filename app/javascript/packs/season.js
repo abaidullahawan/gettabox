782514f66120dc8beb17ca941c53e3b16445166c
Ameer Hamza <ameer.hamza@devbox.co>
2021-12-06 16:25:56 +0500

    Service rules dropdown changing

--- app/javascript/packs/season.js
+++ app/javascript/packs/season.js
@@ -23,4 +23,34 @@ $(document).on('turbolinks:load', function () {
       }
     })
   })
+
+  $('.rule_field').on('change', function () {
+    var field = this.value
+    $.ajax({
+      url: '/mail_service_rules/search_courier_services',
+      context: this,
+      data: { 'rule_field': field },
+      type: "GET",
+      dataType: "json",
+      success: function (response) {
+        var row = this.closest('.row')
+        var rule_operator = $(row).find('.rule_operator')
+        var rule_value = $(row).find('.rule_value')
+        rule_operator.html('')
+        if (response == true) {
+          rule_operator.closest('div').addClass('d-none')
+          rule_value.closest('div').addClass('col-5')
+        }
+        else {
+          rule_value.closest('div').removeClass('col-5')
+          rule_operator.closest('div').removeClass('d-none')
+          $.each(response, function (key, value) {
+            var o = new Option(value, key);
+            $(o).html(value);
+            $(rule_operator).append(o)
+          });
+        }
+      }
+    })
+  })
 })
