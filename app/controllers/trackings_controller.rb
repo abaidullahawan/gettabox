bc65f46fbb99eab1deb1f5725d34cda8d74cbd13
Ameer Hamza <ameer.hamza@devbox.co>
2022-01-20 19:26:24 +0500

    tracking upload fix in order dispatch

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -51,9 +51,9 @@ class TrackingsController < ApplicationController
     end
   end
 
-  def generate_csv(rows, attributes)
+  def generate_csv(rows)
     @csv = CSV.generate(headers: true) do |csv|
-      csv << attributes
+      csv << %w[Reference Name Quantity SKU Tracking Message]
       rows.each do |row|
         csv << row
       end
@@ -67,23 +67,23 @@ class TrackingsController < ApplicationController
   def courier_csv_export(tracking_csv)
     rows = []
     order_ids = params[:tracking_orders].split(',')
-    @export_mapping = ExportMapping.find_by(id: params[:export_id])
-    return redirect_response if @export_mapping.nil?
+    # @export_mapping = ExportMapping.find_by(id: params[:export_id])
+    # return redirect_response if @export_mapping.nil?
 
-    to_be_ignored = %w[id user_type selected created_at updated_at]
-    channel_order_data = {}
-    channel_order_item_data = {}
-    address_data = {}
-    system_user_data = {}
-    mail_service_label_data = {}
-    @export_mapping.mapping_data.compact_blank.each do |key, attribute|
-      channel_order_data[key] = attribute if ChannelOrder.column_names.include? attribute
-      channel_order_item_data[key] = attribute if ChannelOrderItem.column_names.excluding(to_be_ignored).include? attribute
-      address_data[key] = attribute if Address.column_names.excluding(to_be_ignored).include? attribute
-      system_user_data[key] = attribute if SystemUser.column_names.excluding(to_be_ignored).include? attribute
-      mail_service_label_data[key] = attribute if MailServiceLabel.column_names.excluding(to_be_ignored).include? attribute
-    end
-    attributes = channel_order_data.keys + channel_order_item_data.keys + system_user_data.keys + address_data.keys + mail_service_label_data.keys + ['Tracking']
+    # to_be_ignored = %w[id user_type selected created_at updated_at]
+    # channel_order_data = {}
+    # channel_order_item_data = {}
+    # address_data = {}
+    # system_user_data = {}
+    # mail_service_label_data = {}
+    # @export_mapping.mapping_data.compact_blank.each do |key, attribute|
+    #   channel_order_data[key] = attribute if ChannelOrder.column_names.include? attribute
+    #   channel_order_item_data[key] = attribute if ChannelOrderItem.column_names.excluding(to_be_ignored).include? attribute
+    #   address_data[key] = attribute if Address.column_names.excluding(to_be_ignored).include? attribute
+    #   system_user_data[key] = attribute if SystemUser.column_names.excluding(to_be_ignored).include? attribute
+    #   mail_service_label_data[key] = attribute if MailServiceLabel.column_names.excluding(to_be_ignored).include? attribute
+    # end
+    # attributes = channel_order_data.keys + channel_order_item_data.keys + system_user_data.keys + address_data.keys + mail_service_label_data.keys + ['Tracking']
     tracking_csv.each do |row|
       order_id = row['order_id'].nil? ? row['channel_order_id'] : ChannelOrder.find_by(order_id: row['order_id'])&.id
       next unless order_ids.include?(order_id.to_s)
@@ -95,22 +95,25 @@ class TrackingsController < ApplicationController
         note = tracking.save ? tracking.tracking_no : tracking.errors.full_messages
         message << note
       end
-      # row = row.to_h.reject { |key, _| key.nil? }
-      # row['message'] = message
-      # rows << row
       order = ChannelOrder.find_by(id: order_id)
-      order_csv = channel_order_data.values.map { |attr| order.send(attr) }
-      item_csv = channel_order_item_data.values.map { |attr| order.channel_order_items.first.send(attr) }
-      address_csv = address_data.values.map { |attr| order.system_user&.addresses&.find_by(address_title: 'delivery')&.send(attr) }
-      system_user_csv = system_user_data.values.map { |attr| order.system_user&.send(attr) }
-      label_csv = mail_service_label_data.values.map { |attr| order.assign_rule.mail_service_labels.first.send(attr) }
-      rows << order_csv + item_csv + system_user_csv + address_csv + label_csv + message
+      row = []
+      row << order.id << order.buyer_name << order.channel_order_items.pluck(:ordered).sum
+      row << order.channel_order_items.pluck(:sku) << order.trackings&.pluck(:tracking_no) << message
+      rows << row
+    end
+    ids = rows.map { |row| row[0].to_s } # Change the index if row zero index changes
+    (order_ids - ids).each do |order_id|
+      order = ChannelOrder.find_by(id: order_id)
+      row = []
+      row << order.id << order.buyer_name << order.channel_order_items.pluck(:ordered).sum
+      row << order.channel_order_items.pluck(:sku) << order.trackings&.pluck(:tracking_no) << 'Tracking not found'
+      rows << row
     end
     if rows.empty?
       flash[:alert] = "Selected order's tracking not found"
       redirect_to order_dispatches_path(order_filter: 'ready')
-    elsif rows.flatten.any? { |a| a.to_s.include?('must') }
-      generate_csv(rows, attributes)
+    elsif rows.flatten.any? { |a| a.to_s.include?('must') || a.to_s.include?('not found') }
+      generate_csv(rows)
     else
       @pdf_order_items = ChannelOrderItem.where(channel_order_id: order_ids).group_by(&:sku)
       request.format = 'pdf'
