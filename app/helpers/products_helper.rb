674bede79cc51034b3ace1a91d80be163d6ab3ec
abbasanwar158 <abbas.anwar@devbox.co>
2022-02-25 22:50:44 +0500

    if multipack have more then 1 product set deduction units

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ProductsHelper # :nodoc:
+  def multi_products_check(multipack_products)
+    deduction_arr = []
+    multipack_products.each do |multipack_product|
+      deduction_arr.push(multipack_product.child.total_stock.to_i / multipack_product.quantity.to_i)
+    end
+    deduction_arr.min
+  end
 end
