ab368bcaac46899c74bcb17b049c0e07b9da13fb
abbasanwar158 <abbas.anwar@devbox.co>
2022-02-26 14:18:23 +0500

    order_dispatch pagination set and chanel quantity set

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,7 +4,7 @@ module ProductsHelper # :nodoc:
   def multi_products_check(multipack_products)
     deduction_arr = []
     multipack_products.each do |multipack_product|
-      deduction_arr.push(multipack_product.child.total_stock.to_i / multipack_product.quantity.to_i) unless multipack_product.quantity.to_i.zero?
+      deduction_arr.push(multipack_product.child.inventory_balance.to_i / multipack_product.quantity.to_i) unless multipack_product.quantity.to_i.zero?
     end
     deduction_arr.min
   end
