c6e19eaaa1a300d3889afa3dbb8c28ac947929e1
Ameer Hamza <ameer.hamza@devbox.co>
2022-02-28 17:47:13 +0500

    multi allocation and amazon tracking upload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,6 +31,7 @@ require("packs/chosen")
 // require('data-confirm-modal')
 require("packs/product_mapping")
 require("packs/dropdown_search")
+require("packs/order_dispatch")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
