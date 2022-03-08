57d6404926749b93c5bf57d0663dfa5da4b5b86e
Ameer Hamza <ameer.hamza@devbox.co>
2022-03-08 16:26:52 +0500

    skip orders before 9:10 from today

--- app/jobs/amazon_order_job.rb
+++ app/jobs/amazon_order_job.rb
@@ -47,6 +47,7 @@ class AmazonOrderJob < ApplicationJob
       amazon_order.response['payload']['Orders'].each do |order|
         channel_order = ChannelOrder.find_or_initialize_by(order_id: order['AmazonOrderId'],
                                                            channel_type: 'amazon')
+        next if order['PurchaseDate'] < ('2022-03-08T09:10:00Z').to_datetime
         channel_order.order_data = order
         channel_order.created_at = order['PurchaseDate']
         channel_order.order_status = order['OrderStatus']
