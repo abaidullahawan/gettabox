1be79355080ac96b98997d4138e40bdc74638512
mralihamza <ranaalihamza1054@gmail.com>
2022-03-09 19:41:14 +0500

    Fixed reviews for product logs and reorder the view.

--- app/controllers/purchase_deliveries_controller.rb
+++ app/controllers/purchase_deliveries_controller.rb
@@ -124,7 +124,8 @@ class PurchaseDeliveriesController < ApplicationController
       @product = Product.find(detail.product.id)
       @stock = @product.total_stock.to_f + detail.quantity.to_f
       @available_stock = @product.available_stock.to_f + detail.quantity.to_f
-      @product.update(total_stock: @stock, available_stock: @available_stock, change_log: "Purchase Order, #{detail.id}, #{detail.purchase_order.system_user.name}, Purchase Order Recieved, #{(detail.purchase_order.purchase_order_details.last.cost_price.to_i * detail.quantity.to_i)}, #{@stock.to_i}, #{detail.quantity.to_i}")
+      inventory_balance = @stock - @product.unshipped
+      @product.update(total_stock: @stock, available_stock: @available_stock, change_log: "Purchase Order, #{detail.id}, #{detail.purchase_order.system_user.name}, Purchase Order Recieved, #{(detail.purchase_order.purchase_order_details.last.cost_price.to_f * detail.quantity.to_i)}, #{inventory_balance}, #{detail.quantity.to_i}")
     end
   end
 
