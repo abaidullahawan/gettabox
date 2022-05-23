a2764ba6a69118218f5f95e616431aaba303ebf7
mralihamza <ranaalihamza1054@gmail.com>
2022-05-23 13:16:45 +0500

    Files migration

--- app/models/file_one.rb
+++ app/models/file_one.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 # FileOne
+
 class FileOne < ApplicationRecord
 end
--- db/migrate/20220519104320_add_filename_to_filetwos.rb
+++
@@ -1,5 +0,0 @@
-class AddFilenameToFiletwos < ActiveRecord::Migration[6.1]
-  def change
-    add_column :file_twos, :filename, :string
-  end
-end
