9b912e7c873c4a9b2afad6b1cf82a7bb07e5ff3d
mralihamza <ranaalihamza1054@gmail.com>
2022-05-27 17:39:29 +0500

    Insertion in bulk

--- app/jobs/add_csv_data_to_db_job.rb
+++ app/jobs/add_csv_data_to_db_job.rb
@@ -10,35 +10,62 @@ class AddCsvDataToDbJob < ApplicationJob
     mapping_id = _args.last[:mapping_id] || _args.last['mapping_id']
     multifile_mapping_id = _args.last[:multifile_mapping_id] || _args.last['multifile_mapping_id']
     multifile = MultifileMapping.find_by(id: multifile_mapping_id)
-    mapping_headers = ImportMapping.find(mapping_id).mapping_data.compact_blank
+    mapping = ImportMapping.find(mapping_id)
+    mapping_headers = mapping.mapping_data.compact_blank
     spreadsheet1 = CSV.parse(open_spreadsheet(filename1))
     spreadsheet2 = CSV.parse(open_spreadsheet(filename2))
     headers1 = spreadsheet1.first
     headers2 = spreadsheet2.first
-    headers1.map! {|v| v&.gsub('_', ' ').gsub(' ', '_') }
-    headers2.map! {|v| v&.gsub('_', ' ').gsub(' ', '_') }
-    array1 = Array.new(headers1.count) { |i| "column_#{i}"}
-    array2 = Array.new(headers2.count) { |i| "column_#{i}"}
+    headers1.map! { |v| v&.gsub('_', ' ')&.gsub(' ', '_') }
+    headers2.map! { |v| v&.gsub('_', ' ')&.gsub(' ', '_') }
+    array1 = Array.new(headers1.count) { |i| "column_#{i}" }
+    array2 = Array.new(headers2.count) { |i| "column_#{i}" }
     headers1 = Hash[array1.zip headers1]
     headers2 = Hash[array2.zip headers2]
     FileOne.where(filename: filename1).first.update(headers1)
     FileTwo.where(filename: filename2).first.update(headers2)
     column_name1 = FileOne.where(filename: filename1).first.attributes.compact_blank.invert[mapping_headers.keys.first]
+
+    batch_size = 100
+    batch_data_for_first_file = []
+    batch_data_for_second_file = []
+
+    # Start insertion in Bulk For 1st File
     spreadsheet1.drop(1).each do |record|
       hash = Hash[array1.zip record]
-      value = hash[column_name1]
-      hash[column_name1] = value&.gsub(/[^0-9A-Za-z]/, '')&.upcase
-      file_one = FileOne.create(filename: filename1)
-      file_one.update(hash)
+      if mapping.mapping_rule.present?
+        value = hash[column_name1]
+        hash[column_name1] = value&.gsub(/[^0-9A-Za-z]/, '')&.upcase
+      end
+      hash.merge!('filename' => filename1)
+      batch_data_for_first_file << hash
+      if batch_data_for_first_file.size == batch_size
+        FileOne.import batch_data_for_first_file
+        batch_data_for_first_file = []
+      end
     end
+    FileOne.import batch_data_for_first_file
+    # End insertion in Bulk For 1st File
+
     column_name2 = FileTwo.where(filename: filename2).first.attributes.compact_blank.invert[mapping_headers.values.first]
+
+    # Start insertion in Bulk For 2nd File
     spreadsheet2.drop(1).each do |record|
       hash = Hash[array2.zip record]
-      value = hash[column_name2]
-      hash[column_name2] = value&.gsub(/[^0-9A-Za-z]/, '')&.upcase
-      file_two = FileTwo.create(filename: filename2)
-      file_two.update(hash)
+      if mapping.mapping_rule.present?
+        value = hash[column_name2]
+        hash[column_name2] = value&.gsub(/[^0-9A-Za-z]/, '')&.upcase
+      end
+      hash.merge!('filename' => filename2)
+      batch_data_for_second_file << hash
+      if batch_data_for_second_file.size == batch_size
+        FileTwo.import batch_data_for_second_file
+        batch_data_for_second_file = []
+      end
     end
+    FileOne.import batch_data_for_second_file
+    # End insertion in Bulk For 2nd File
+
     delete_files(filename1)
     delete_files(filename2)
     job_data = MultiFileMappingJob.perform_later(
