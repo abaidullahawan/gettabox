594759371c82062b423c7e570a43fa0aa631c253
Ameer Hamza <ameer.hamza@devbox.co>
2022-05-30 19:16:16 +0500

    fix waittime job

--- app/jobs/waiting_time_job.rb
+++ app/jobs/waiting_time_job.rb
@@ -7,7 +7,7 @@ class WaitingTimeJob < ApplicationJob
   def perform(*_args)
     job_status_id = _args.last[:job_status_id] || _args.last['job_status_id']
     job_status = JobStatus.find_by(id: job_status_id)
-    return unless job_status.present? && job_status.status_busy?
+    return unless job_status.present? && !job_status.status_busy?
 
     arguments = job_status.arguments.nil? ? {} : job_status.arguments
     job = job_status.name.constantize.set(wait: job_status.perform_in.seconds).perform_later(arguments.merge(job_status_id: job_status_id))
