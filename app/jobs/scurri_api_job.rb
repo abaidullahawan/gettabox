099d939247341305fc4e86462bf4d98f27a09a22
mralihamza <ranaalihamza1054@gmail.com>
2022-07-28 16:06:50 +0500

    Tracking remove.

--- app/jobs/scurri_api_job.rb
+++ app/jobs/scurri_api_job.rb
@@ -18,52 +18,56 @@ class ScurriApiJob < ApplicationJob
 
     data =
       channel_orders.each.map do |order|
+        mail_service_rule = order.assign_rule.mail_service_rule
+        courier_name = mail_service_rule.courier.name
+        service_name = mail_service_rule.service.name
+        next unless courier_name.eql? 'ITD'
+
         customer = order.system_user
         delivery_address = customer.addresses.where(address_title: 'delivery').first
-        tracking = order.trackings.first
-      {
-        "carrier" => tracking.carrier,
-        "warehouse_id" => "itd-global-gettabox-ltd|ITD Global: GETTABOX LTD",
-        "service_id" => carrier_shipping_service(tracking.shipping_service),
-        "recipient" => {
-          "address" => {
-            "country" => delivery_address.country,
-            "postcode" => delivery_address.postcode,
-            "city" => delivery_address.city,
-            "address1" => delivery_address.address,
-            "state" => delivery_address.region
+        {
+          'carrier' => courier_name,
+          'warehouse_id' => 'itd-global-gettabox-ltd|ITD Global: GETTABOX LTD',
+          'service_id' => carrier_shipping_service(service_name),
+          'recipient' => {
+            'address' => {
+              'country' => delivery_address.country,
+              'postcode' => delivery_address.postcode,
+              'city' => delivery_address.city,
+              'address1' => delivery_address.address,
+              'state' => delivery_address.region
+            },
+            'contact_number' => customer.phone_number,
+            'email_address' => customer.email,
+            'company_name' => delivery_address.company,
+            'name' => delivery_address.name
           },
-          "contact_number" => customer.phone_number,
-          "email_address" => customer.email,
-          "company_name" => delivery_address.company,
-          "name" => delivery_address.name
-        },
-        "order_number" => order.order_id,
-        "options" => {
-          "package_type" => "Standard parcel",
-          "signed" => "no"
-        },
-        "packages" =>
-          order.assign_rule.mail_service_labels.each.map do |label|
-          {
-            "items" => [
+          'order_number' => order.order_id,
+          'options' => {
+            'package_type' => 'Standard parcel',
+            'signed' => 'no'
+          },
+          'packages' =>
+            order.assign_rule.mail_service_labels.each.map do |label|
               {
-                "weight" => label.weight / 1000,
-                "value" => order.total_amount,
-                "fabric_content" => "Household",
-                "country_of_origin" => "GB",
-                "sku" => order.channel_order_items.first&.sku,
-                "quantity" => order.channel_order_items.first&.ordered,
-                "name" => order.channel_order_items.first&.title
+                'items' => [
+                  {
+                    'weight' => label.weight / 1000,
+                    'value' => order.total_amount,
+                    'fabric_content' => 'Household',
+                    'country_of_origin' => 'GB',
+                    'sku' => order.channel_order_items.first&.sku,
+                    'quantity' => order.channel_order_items.first&.ordered,
+                    'name' => order.channel_order_items.first&.title
+                  }
+                ],
+                'length' => label.length,
+                'height' => label.height,
+                'width' => label.width,
+                'reference' => 'CustomCustomerReference1'
               }
-            ],
-            "length" => label.length,
-            "height" => label.height,
-            "width" => label.width,
-            "reference" => "CustomCustomerReference1"
-          }
-          end
-      }
+            end
+        }
       end
 
     response = ScurriApiService.create_shipment(url, data, auth)
@@ -108,23 +112,13 @@ class ScurriApiJob < ApplicationJob
 
   def carrier_shipping_service(shipping_service)
     shipping_services = {
-      'yodel direct xpress mini' => 'Yodel|XPRESS MINI 48 NON POD 2CMN',
-      'yodel direct xpress 48 pod' => 'Yodel|XPRESS 48 POD 2CP',
-      'yodel direct xpress 48 non pod' => 'Yodel|XPRESS 48 POD 2CP',
-      'yodel direct xpress 24 pod' => 'Yodel|XPRESS 24 POD 1CP',
-      'yodel direct xpress 24 non pod' => 'Yodel|XPRESS 24 POD 1CP',
-      'yodel direct xpect mon - sat pod' => 'Yodel|XPECT 24 POD 1VP',
-      'yodel direct xpect mon - sat non pod' => 'Yodel|XPECT 48 POD 2VP',
-      'yodel direct xpect 24 pod' => 'Yodel|XPECT 24 POD 1VP',
-      'yodel direct xpect 48 pod' => 'Yodel|XPECT 48 POD 2VP',
-      'yodel direct xpect 48 non pod' => 'Yodel|XPECT 48 POD 2VP',
-      'yodel direct channel islands 48 non pod' => 'Yodel|XPECT 48 POD 2VP',
-      'hermes 48' => 'Hermes|UK Standard Delivery',
-      'hermes 24' => 'Hermes|UK Standard Delivery',
-      'hermes 48 pod' => 'Hermes|UK Standard Delivery',
-      'hermes 24 pod' => 'Hermes|UK Standard Delivery',
-      'hermes 48 non pod' => 'Hermes|UK Standard Delivery',
-      'hermes 24 non pod' => 'Hermes|UK Standard Delivery',
+      'UK Standard Delivery' => 'Hermes|UK Standard Delivery',
+      'UK Next Day Delivery' => 'Hermes|UK Next Day Delivery',
+      'XPECT 24 POD (1VP)' => 'Yodel|XPECT 24 POD 1VP',
+      'XPECT 48 POD (2VP)' => 'Yodel|XPECT 48 POD 2VP',
+      'XPRESS 24 POD (1CP)' => 'Yodel|XPRESS 24 POD 1CP',
+      'XPRESS 48 POD (2CP)' => 'Yodel|XPRESS 48 POD 2CP',
+      'XPRESS MINI 48 NON POD (2CMN)' => 'Yodel|XPRESS MINI 48 NON POD 2CMN'
     }
     shipping_services[shipping_service]
   end
